b5931ac076176dd453ed9be4c7d8db00d3e28d24
swati kumari <swatisinghaniya702@gmail.com>
2020-07-17 18:22:09 +0530

    Updates

---
+++ precache-manifest.d8735e969c9127658c6326d935f10707.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e39a3667778223268e38c0e8857c89",
+    "url": "/SatishConcultantApp/index.html"
+  },
+  {
+    "revision": "c366396b2f9de8784da1",
+    "url": "/SatishConcultantApp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4dea537e3f556150d68d",
+    "url": "/SatishConcultantApp/static/css/main.02b5fe49.chunk.css"
+  },
+  {
+    "revision": "c366396b2f9de8784da1",
+    "url": "/SatishConcultantApp/static/js/2.d375f490.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/SatishConcultantApp/static/js/2.d375f490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dea537e3f556150d68d",
+    "url": "/SatishConcultantApp/static/js/main.33d66833.chunk.js"
+  },
+  {
+    "revision": "3cadc8133e8cb4153417",
+    "url": "/SatishConcultantApp/static/js/runtime-main.794b8712.js"
+  },
+  {
+    "revision": "25da23037207b260c077a5579714ab19",
+    "url": "/SatishConcultantApp/static/media/android.25da2303.jpg"
+  },
+  {
+    "revision": "f184909435a46204e4101ebd4a344a4d",
+    "url": "/SatishConcultantApp/static/media/app.f1849094.jpg"
+  },
+  {
+    "revision": "67bad01c2ec4619786a3b081c211ff54",
+    "url": "/SatishConcultantApp/static/media/digital.67bad01c.jpg"
+  },
+  {
+    "revision": "951e429f93244d83e72d04c709262616",
+    "url": "/SatishConcultantApp/static/media/logo1.951e429f.png"
+  },
+  {
+    "revision": "d6a929f160d550d726b2f5ebb96bce98",
+    "url": "/SatishConcultantApp/static/media/logo2.d6a929f1.png"
+  },
+  {
+    "revision": "561a8f51d656dd30ccf7eebb93de471f",
+    "url": "/SatishConcultantApp/static/media/marketing.561a8f51.jpg"
+  },
+  {
+    "revision": "2ffedeeb13be4dc6e4fc5d6cb30a307e",
+    "url": "/SatishConcultantApp/static/media/service.2ffedeeb.jpg"
+  },
+  {
+    "revision": "c222e8ebef8d6703295f4f2fbb01ee12",
+    "url": "/SatishConcultantApp/static/media/soft.c222e8eb.jpg"
+  },
+  {
+    "revision": "547379c7c2ff335d43213e7e2ad2fb37",
+    "url": "/SatishConcultantApp/static/media/web.547379c7.jpg"
+  }
+]);
